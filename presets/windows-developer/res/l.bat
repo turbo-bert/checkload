bbb483c7afbe6e3cb7d4f912aecdde1cf7917ee2
Robert <turbobert-github@mailbox.org>
2022-06-02 16:00:45 +0200

    ...

--- presets/windows-developer/res/l.bat
+++ presets/windows-developer/res/l.bat
@@ -3,9 +3,9 @@
 echo I put this toplevel on C so it is easy to include by just running "/l"
 
 set SHELL="C:\Program Files\Emacs\emacs-28.1\libexec\emacs\28.1\x86_64-w64-mingw32\cmdproxy.exe"
-set PATH=C:\tmp\bin;C:\lazarus\fpc\3.2.2\bin\x86_64-win64;"C:\Program Files\Emacs\emacs-28.1\bin";%PATH%
+set PATH=C:\tmp\bin;C:\lazarus\fpc\3.2.2\bin\x86_64-win64;%PATH%
 
 mkdir C:\tmp
 mkdir C:\tmp\bin
 
-echo emacs -nw %1 >C:\tmp\bin\e.bat
+echo "C:\Program Files\Emacs\emacs-28.1\bin\emacs" -nw %1 >C:\tmp\bin\e.bat
